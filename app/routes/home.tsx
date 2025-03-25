a009204c60ad0239fce44263dad2eeb9ad05b958
Chance Strickland <hi@chance.dev>
2025-03-25 11:36:11 -0600

    window junk

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Player } from "../ui/player";
+import { Window } from "../ui/window";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Player />;
+  return (
+    <Window title="Media Player">
+      <Player />
+    </Window>
+  );
 }
